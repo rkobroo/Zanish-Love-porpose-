cf49d031c754ef3482d73e78d227c94401d729b1
rkobroo <156598034+rkobroo@users.noreply.github.com>
2026-02-05 11:19:28 +0545

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Samita'; // Replace with your secret code
+const secretCode = 'Dallu'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
@@ -16,3 +16,4 @@ form.addEventListener('submit', (e) => {
 		resultDiv.innerHTML = 'Tait tmi mero baby haineu ko hau tmi 😎';
 	}
 });
+
